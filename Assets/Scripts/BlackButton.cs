1cd1d55bdecd81c5f7b8583c2a3e9011e453f277
James Boon <61937112+JimmyBoon@users.noreply.github.com>
2021-07-01 12:54:53 +1000

    Black button destroys
    Black button destroys all shapes

--- Assets/Scripts/BlackButton.cs
+++ Assets/Scripts/BlackButton.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class BlackButton : ButtonControl
 {
+
     private void Start()
     {
         image.color = Color.black;
+    }
+
+    public void DestroyAll()
+    {
+        Shape[] shapes = FindObjectsOfType<Shape>();
+
+        foreach(Shape shape in shapes)
+        {
+            Destroy(shape.transform.gameObject);
+        }
     }
 
 }
