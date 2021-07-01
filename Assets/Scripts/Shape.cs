65825ae48187fbf30c77be20b66e641da89bd3d4
James Boon <61937112+JimmyBoon@users.noreply.github.com>
2021-07-01 12:47:33 +1000

    Changing Colour is working

--- Assets/Scripts/Shape.cs
+++ Assets/Scripts/Shape.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class Shape : MonoBehaviour
 {
+    Material material;
+    GameManager manager;
+
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+        
+    }
 
     
     void OnMouseDown()
     {
         DisplayText();
+        ChangeColor();
     }
 
     protected virtual void DisplayText()
     {
         Debug.Log(gameObject.name);
+    }
+
+    protected virtual void ChangeColor()
+    {
+        manager = FindObjectOfType<GameManager>();
+        material.color = manager.SelectedColor;
     }
 
 }
